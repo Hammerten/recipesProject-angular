094a4af664911ca038dafddb4f6dd416f6c25eea
Hammerten <gvalishvili@gmail.com>
2021-02-02 23:21:24 +0400

    add ShoppingListComponent

---
+++ src/app/shopping-list/shopping-list.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { Ingredient } from '../models/ingredient.model';
+
+@Component({
+  selector: 'app-shopping-list',
+  templateUrl: './shopping-list.component.html',
+  styleUrls: ['./shopping-list.component.css']
+})
+
+export class ShoppingListComponent {
+  ingredients: Ingredient[] = [
+    new Ingredient('Apples', 5),
+    new Ingredient('Tomatoes', 10),
+  ];
+
+  addIngredient(ingredient: Ingredient): void {
+    this.ingredients.push(ingredient);
+  }
+}
