a7832755dcb7c88dc666a3f232fd15c8cf153759
Hammerten <gvalishvili@gmail.com>
2021-02-02 23:09:43 +0400

    add recipes component

---
+++ src/app/recipes/recipes.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import {Recipe} from '../models/recipe.model';
+
+@Component({
+  selector: 'app-recipes',
+  templateUrl: './recipes.component.html',
+  styleUrls: ['./recipes.component.css']
+})
+export class RecipesComponent implements OnInit {
+  selectedRecipe: Recipe | undefined;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onRecipeSelectFiredFromChild(value: Recipe): void {
+    this.selectedRecipe = value;
+  }
+}
