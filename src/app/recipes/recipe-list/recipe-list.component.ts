bf945848fa7cc4de7a76dd3a653fdc55b92a6939
Hammerten <gvalishvili@gmail.com>
2021-02-02 23:12:51 +0400

    add RecipeListComponent

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,26 @@
+import {Component, OnInit, Output, EventEmitter} from '@angular/core';
+import { Recipe } from '../../models/recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+  @Output() recipeSelectFiredFromChild = new EventEmitter<Recipe>();
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'test1', 'https://www.sugarandsoul.co/wp-content/uploads/2018/01/hungarian-goulash-recipe-17.jpg'),
+    new Recipe('A Test Recipe', 'test2', 'https://www.sugarandsoul.co/wp-content/uploads/2018/01/hungarian-goulash-recipe-17.jpg'),
+    new Recipe('A Test Recipe', 'test3', 'https://www.sugarandsoul.co/wp-content/uploads/2018/01/hungarian-goulash-recipe-17.jpg')
+  ];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onRecipeSelectFired(recipe: Recipe): void {
+    this.recipeSelectFiredFromChild.emit(recipe);
+  }
+
+}
