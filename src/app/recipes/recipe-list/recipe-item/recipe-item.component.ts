32ab1b02c7b744568529e9554849b41708256090
Hammerten <gvalishvili@gmail.com>
2021-02-02 23:17:12 +0400

    add RecipeItemComponent

---
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Recipe } from '../../../models/recipe.model';
+
+@Component({
+  selector: 'app-recipe-item',
+  templateUrl: './recipe-item.component.html',
+  styleUrls: ['./recipe-item.component.css']
+})
+export class RecipeItemComponent implements OnInit {
+  @Input() recipe: Recipe | undefined;
+  @Output() recipeSelectFired = new EventEmitter<void>();
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onRecipeSelect(): void {
+    this.recipeSelectFired.emit();
+  }
+}
